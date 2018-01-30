10a414c06790ead8645f1e8ded1359c870ce8caf
Mohaminul Kabir Khan <Itsmishat@gmail.com>
2018-01-31 00:23:41 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,64 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
 |
 */
 
+Auth::routes();
+
+//////////////////Welcomedefault///////////////
 Route::get('/', function () {
     return view('welcome');
 });
+//////////////////HomeSweetHome///////////////
+Route::get('home', function () {
+    return view('home');
+});
+/////////////////Profileroutes///////////////////////////
+Route::get('profile', ['uses'=>'ProfileController@profile']);
+Route::post('profile', ['uses'=>'ProfileController@update']);
+
+//////////////////WelcomeController///////////////
+Route::get('welcome',['uses'=>'WelcomeController@welcome']);
+ 
+//////////////////UsersController///////////////
+
+Route::get('create', ['uses'=>'UsersController@create']);
+Route::post ('users/ucreate', ['uses' => 'UsersController@store']);
+Route:: get('users/uview',['uses' =>'UsersController@uview']);
+
+/////////////////FormController///////////////////////
+Route::get('cform',['uses' => 'CformController@cform']);
+//Route::post('cform',['uses' => 'CformController@store']);
+Route::post('cform', ['uses'=> 'CformController@upload']);
+
+//////////////Nomination for President/////////////
+Route::get('cform/cpview',['uses' => 'cviewcontroller@cpview']);
+Route::post('/cform/cpview',['uses' => 'Candidatescontroller@store']);
+
+//////////////Nomination for Vice-President/////////////////
+Route::get('cform/cvpview',['uses' => 'cviewcontroller@cvpview']);
+Route::post('cform/cvpview',['uses' => 'Candidatescontroller@store']);
+
+//////////////////sorry//////////////////////////////
+Route::get('supreme', ['uses' => 'SupremeController@Supreme']);
+Route::get('sorry', ['uses'=>'SorryController@sorry']);
+
+///////////////for election panel////////////////////////////////////Vote//////////////////
+Route::get('/election/epanel', ['uses' => 'EpanelController@epanel']);
+Route::post('/election/epanel', ['uses' => 'EpanelController@election']);
+
+//////////////For guest messages////////////
+Route::post('/welcome', ['uses' => 'g_messageController@message']);
+Route::get('/users/g_messages', ['uses' => 'g_messageController@g_message']);
+//////////For vote//////////
+Route:: get('/election/vote_p', ['uses' => 'VoteController@vote_p']);
+Route:: get('/election/vote_vp', ['uses' => 'VoteController@vote_vp']);
+Route::post('/election/vote_p', ['uses' => 'VoteController@S_votes']);
+Route::post('/election/vote_vp', ['uses' => 'VoteController@S_votes']);
+
+////////////////For Results//////////////////////////
+Route::get('results', ['uses' => 'ResultCOntroller@result']);
